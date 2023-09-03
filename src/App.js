50e10e9227ea4c15b5b3a5371b9bb16664fe3254
JoakoGomez10 <joaquingomezaj@gmail.com>
2023-09-03 18:11:32 -0300

    Se agrego bootstrap y componente login.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import { Login } from './components/login/Login';
 
 function App() {
   return (
     <div className="container">
-      <div className='row'>
-          <div className='col'>
-              <h1>Bienvenido a react con bootstrap</h1>
+      <div className='row mb-5'>
+          <div className='col mb-5'>
+              <h1></h1>
           </div>
+      </div>
+      <div className='row'>
+        <div className='col'>
+            <Login/>
+        </div>
 
       </div>
     </div>
---
+++ src/components/login/Login.js
@@ -0,0 +1,36 @@
+import React from 'react'
+// Importing the Bootstrap 5 CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const Login = () => {
+  return (
+        <div class="row d-flex justify-content-center">
+          <div class="col-12 col-md-8 col-lg-6">
+            <div class="card bg-white border-0">
+              <div class="card-body p-4 ">
+                <form class="mb-3 mt-md-4">
+                  <h2 class="mb-3 text-center">Iniciar Sesion</h2>
+                  <div class="mb-3">
+                    <label for="email" class="form-label ">Correo electronico</label>
+                    <input type="email" class="form-control" id="email" placeholder="name@example.com"></input>
+                  </div>
+                  <div class="mb-3">
+                    <label for="password" class="form-label ">Contraseña</label>
+                    <input type="password" class="form-control" id="password" placeholder="*******"></input>
+                  </div>
+                  <p class="small"><a class="text-primary" href="forget-password.html">Forgot password?</a></p>
+                  <div class="d-grid">
+                    <button class="btn btn-outline-dark" type="submit">Ingresar</button>
+                  </div>
+                </form>
+                <div>
+                  <p class="mb-0  text-center">No tienes cuenta? <a href="signup.html" class="text-primary fw-bold">Crea 
+                      una</a></p>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+  )
+}
